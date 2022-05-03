2413976e02f67bc879ec59074bce39a26efeb9a2
DMeier12 <102044243+DMeier12@users.noreply.github.com>
2022-05-02 19:17:33 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.easeIn', { duration: 2000, delay: 400 });
+ScrollReveal().reveal('.easeIn-slow', { duration: 1500, delay: 1300 });
+ScrollReveal().reveal('.easeIn-medium', { duration: 1500, delay: 700 });
